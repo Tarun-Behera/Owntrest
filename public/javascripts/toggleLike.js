96d022bbc05b8959f5190e470e97604c2b474428
Tarun Behera <starbehera6@gmail.com>
2024-09-20 16:37:38 +0530

    added minor ui changes for responsive of small device

--- public/javascripts/toggleLike.js
+++ public/javascripts/toggleLike.js
@@ -23,16 +23,16 @@ async function toggleLike(likeBtn, event) {
             "opacity-0",
             "group-hover:opacity-100",
             "hover:text-red-500",
-            "text-[rgba(225,225,225,0.7)]"
+            "text-[rgba(164,164,164,0.6)]"
           );
           postLikeBtn.classList.remove("text-red-500", "opacity-100");
         }
         if (sclLikeBtn) {
           sclLikeBtn.setAttribute("data-liked", "false");
-          sclLikeBtn.classList.add("text-[rgba(225,225,225,0.7)]");
+          sclLikeBtn.classList.add("text-[rgba(164,164,164,0.6)]");
           sclLikeBtn.classList.remove("text-red-500", "opacity-100");
         }
-        msgCtr.innerHTML = `<div class="unlikedMessage absolute transition-all ease-out duration-[500ms] bottom-[-50px] left-[50%] translate-x-[-50%] bg-[#2b2b2b] rounded-[10px] p-4 z-[99999]">Removed from My Likes</div>`;
+        msgCtr.innerHTML = `<div class="unlikedMessage absolute transition-all ease-out duration-[500ms] bottom-[-50px] left-[50%] translate-x-[-50%] bg-[#2b2b2b] rounded-[10px] w-[220px] sm:w-auto p-3 sm:p-4 z-[99999]">Removed from My Likes</div>`;
         msgCtr.classList.remove("hidden");
         // Trigger the animation by changing the bottom position
         setTimeout(() => {
@@ -47,8 +47,7 @@ async function toggleLike(likeBtn, event) {
         // Finally, remove the message from the DOM
         setTimeout(() => {
           document.querySelector(".unlikedMessage").remove();
-        }, 1650);
-        // console.log("Removed from My Likes folder");
+        }, 1600);
       } else {
         // like it
         if (postLikeBtn) {
@@ -71,7 +70,7 @@ async function toggleLike(likeBtn, event) {
           );
           sclLikeBtn.classList.add("text-red-500", "opacity-100");
         }
-        msgCtr.innerHTML = `<div class="likedMessage absolute bottom-[-50px] left-[50%] translate-x-[-50%] transition-all duration-[500ms] ease-out bg-[#2b2b2b] rounded-[10px] p-4 z-[99999]"><i class="ri-heart-3-fill text-red-500 mx-2"></i>Added to My Likes</div>`;
+        msgCtr.innerHTML = `<div class="likedMessage absolute bottom-[-50px] left-[50%] translate-x-[-50%] transition-all duration-[500ms] ease-out bg-[#2b2b2b] rounded-[10px] w-[215px] sm:w-auto p-3 sm:p-4 z-[99999]"><i class="ri-heart-3-fill text-red-500 mx-2"></i>Added to My Likes</div>`;
         msgCtr.classList.remove("hidden");
 
         // Trigger the animation by changing the bottom position
@@ -83,12 +82,11 @@ async function toggleLike(likeBtn, event) {
         setTimeout(() => {
           document.querySelector(".likedMessage").classList.add("opacity-0");
         }, 1500);
-        // console.log("Added to My Likes folder");
 
         // Finally, remove the message from the DOM
         setTimeout(() => {
           document.querySelector(".likedMessage").remove();
-        }, 1650);
+        }, 1600);
       }
     } else {
       // console.error("Failed to toggle like");
