a10fa313e7e6cb184681668c016255eed9c66fa3
Tarun Behera <starbehera6@gmail.com>
2024-10-15 21:26:21 +0530

    new animation for home page

---
+++ public/javascripts/animation-1.js
@@ -0,0 +1,53 @@
+const lenis = new Lenis({
+  duration: 4,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+gsap.registerPlugin(ScrollTrigger);
+document.querySelectorAll(".item").forEach((elem) => {
+  let image = elem.querySelector("img");
+  let t1 = gsap.timeline();
+  let xTransform = gsap.utils.random(-100, 100);
+  t1.set(
+    image,
+    {
+      transformOrigin: `${xTransform < 0 ? 0 : "100%"}`,
+    },
+    "start"
+  )
+    .to(
+      image,
+      {
+        scale: 0,
+        duration: 6,
+        ease: "none",
+        scrollTrigger: {
+          trigger: image,
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      },
+      "start"
+    )
+    .to(
+      elem,
+      {
+        xPercent: xTransform,
+        scrollTrigger: {
+          trigger: image,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+        },
+      },
+      "start"
+    );
+});
+
+
---
+++ public/javascripts/animation-2.js
@@ -0,0 +1,55 @@
+const lenis = new Lenis({
+    duration: 4,
+  });
+  
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  
+  requestAnimationFrame(raf);
+  gsap.registerPlugin(ScrollTrigger);
+document.querySelectorAll(".item").forEach((elem) => {
+    let image = elem.querySelector("img");
+    let t1 = gsap.timeline();
+    let xTransform = gsap.utils.random(-100, 100);
+    t1.set(
+      image,
+      {
+        transformOrigin: `${xTransform < 0 ? 0 : "100%"}`,
+      },
+      "start"
+    );
+    gsap
+      .fromTo(
+        image,
+        { scale: 0.7 }, // Starting scale
+        {
+          scale: 1, // End scale
+          duration: 6,
+          ease: "none",
+          scrollTrigger: {
+            trigger: image,
+            start: "top bottom", // Animation starts when image comes into view
+            end: "top top", // Animation ends when the image reaches the top
+            scrub: true, // Smoothly scrub the animation as you scroll
+            onLeaveBack: () => gsap.to(image, { scale: 0.7 }), // Scale up when scrolling back up
+            onEnterBack: () => gsap.to(image, {}), // Scale down when reaching the top again
+          },
+        },
+        "start"
+      )
+      t1.to(
+        elem,
+        {
+          xPercent: xTransform,
+          scrollTrigger: {
+            trigger: image,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+          },
+        },
+        "start"
+      );
+  });
